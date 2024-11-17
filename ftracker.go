ca3c369fe3aa3a166388abc4cf6be663e457e0f6
Andrei <resnenkoandrej@gmail.com>
2024-11-17 18:08:03 +0700

    Я вставил деление на 3.6 в функции WalkingSpentCalories , чтобы перевести км/ч в м/с , потому что константа дает менее точные результаты. Сейчас при запуке теста ответ почти верный, на 0.06 погрешность. В связи с чем эта пограшеность, я не знаю, я переписывал этот код несколько раз, подключал чат GPT в решение и код я не знаю как еще по другому написать, нужна какая то наводка. Мой наставник сказал все хорошо... И по функциии SwimmingSpentCalories я также все пересмотрел, и написал ее в соотвесвии с инсструкцией, своими мозгами и затем чат GPT, я не понимаю что не так,

--- ftracker.go
+++ ftracker.go
@@ -59,7 +59,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Плавание":
 		distance := distance(action)                                               // вызовите здесь необходимую функцию
-		speed := swimmingMeanSpeed(lengthPool, countPool, duration)                // вызовите здесь необходимую функцию
+		speed := meanSpeed(action, duration)                                       // вызовите здесь необходимую функцию
 		calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	default:
@@ -101,8 +101,8 @@ const (
 // weight float64 — вес пользователя.
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
-	// ваш код здесь
-	spentCalories := ((walkingCaloriesWeightMultiplier*weight + math.Sqrt((meanSpeed(action, duration)/3.6))/(height/100)) * walkingSpeedHeightMultiplier * weight) * duration * minInH
+	spentCalories := ((walkingCaloriesWeightMultiplier * weight) +
+		((math.Pow(meanSpeed(action, duration)/3.6, 2) / (height / cmInM)) * walkingSpeedHeightMultiplier * weight)) * duration * minInH
 	return spentCalories
 }
 
@@ -135,7 +135,7 @@ func swimmingMeanSpeed(lengthPool, countPool int, duration float64) float64 {
 // duration float64 — длительность тренировки в часах.
 // weight float64 — вес пользователя.
 func SwimmingSpentCalories(lengthPool, countPool int, duration, weight float64) float64 {
-	// ваш код здесь
-	spentCalories := (swimmingMeanSpeed(lengthPool, countPool, duration) + swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * weight * duration
+	speed := swimmingMeanSpeed(lengthPool, countPool, duration) // средняя скорость
+	spentCalories := (speed + swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * weight * duration
 	return spentCalories
 }
